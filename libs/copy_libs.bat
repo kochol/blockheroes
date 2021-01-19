bb64c2e16c160e286883c4528143d6a45bfd4cc1
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-01-19 11:42:06 +0800

    Update copy_libs.bat

--- libs/copy_libs.bat
+++ libs/copy_libs.bat
@@ -1,1 +1,2 @@
 copy ..\build\Debug_armv7-none-linux-android21\block-heroes\libblock-heroes.a android\armeabi-v7a
+copy ..\build\Debug_arm64-none-linux-android21\block-heroes\libblock-heroes.a android\arm64-v8a

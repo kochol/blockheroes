86e51d2f764a478d6c977f8336c542bab98c8199
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-01-19 11:30:52 +0800

    add android lib files

--- libs/copy_libs.bat
+++ libs/copy_libs.bat
@@ -1,3 +1,1 @@
-copy ..\build\Debug_armv7-none-linux-android21\block-heroes\libblock-heroes.a .
-copy ..\..\fips-build\ariyana\android-ninja-debug\src\c99\libcari.so .
-copy ..\..\fips-build\ariyana\android-ninja-debug\src\c99\libcari.so ..\..\fips-build\block-heroes\android-ninja-debug\android\block_heroes\lib\armeabi-v7a
+copy ..\build\Debug_armv7-none-linux-android21\block-heroes\libblock-heroes.a android\armeabi-v7a
